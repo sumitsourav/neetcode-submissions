a3b8aa8f39aba56ccce27be6de14877d3fb08c81
Sumit Sourav <su5mitsourav@gmail.com>
2026-06-26 19:44:13 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.go
@@ -0,0 +1,13 @@
+func twoSum(nums []int, target int) []int {
+    lookup := make(map[int]int)
+
+    for i, num := range nums {
+        complement := target - num
+        idx, exists := lookup[complement]
+        if exists {
+            return []int{idx,i}
+        }
+        lookup[num] = i
+    }
+    return []int{}
+}
